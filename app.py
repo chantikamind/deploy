f77c33ea938ed2c908172a64ce199813ace40ebb
chantikamind <cantikapurdiyanto88@gmail.com>
2025-12-11 22:07:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -525,4 +525,4 @@ def health_check():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=True)
